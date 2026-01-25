213651f0843e1193e912476de4aa09f2860bc54c
mohamedtaha <mohamedtahamaxab@users.noreply.github.com>
2026-01-25 17:08:20 +0200

    Replace icon buttons with text buttons.

--- app/src/main/java/com/example/nabdtask/presentation/detail/DetailScreen.kt
+++ app/src/main/java/com/example/nabdtask/presentation/detail/DetailScreen.kt
@@ -7,9 +7,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -35,7 +35,7 @@ fun DetailScreen(
             TopAppBar(
                 title = { Text("Notification") },
                 actions = {
-                    IconButton(onClick = { onCancel(notification) }) {
+                    TextButton(onClick = { onCancel(notification) }) {
                         Text("Cancel")
                     }
                 },
--- app/src/main/java/com/example/nabdtask/presentation/main/NotificationsScreen.kt
+++ app/src/main/java/com/example/nabdtask/presentation/main/NotificationsScreen.kt
@@ -11,9 +11,9 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -61,7 +61,7 @@ fun NotificationsScreen(
             TopAppBar(
                 title = { Text("Notifications") },
                 actions = {
-                    IconButton(onClick = { onCancelAll() }) {
+                    TextButton(onClick = { onCancelAll() }) {
                         Text("Cancel All")
                     }
                 },
